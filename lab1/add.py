c47cf12a81c36ea92b511cd5d8b9eeaeaa0185f6
creation7654321 <creation.pradhan@gmail.com>
2025-03-22 21:16:44 +0545

    adding 4 number

--- lab1/add.py
+++ lab1/add.py
@@ -8,3 +8,17 @@ num3 = 30
 
 result = add_three_numbers(num1, num2, num3)
 print("The sum is:", result)
+
+
+# Function to add four numbers
+def add_four_numbers(a, b, c, d):
+    return a + b + c + d
+
+# Example usage
+num1 = 10
+num2 = 20
+num3 = 30
+num4 = 40
+
+result = add_four_numbers(num1, num2, num3, num4)
+print("The sum is:", result)
