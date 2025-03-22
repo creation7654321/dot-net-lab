d50658100b858b413ca2aa7cef5785aecf59b5f7
creation7654321 <creation.pradhan@gmail.com>
2025-03-22 21:11:04 +0545

    first commit

---
+++ lab1/add.py
@@ -0,0 +1,10 @@
+def add_three_numbers(a, b, c):
+    return a + b + c
+
+# Example usage
+num1 = 10
+num2 = 20
+num3 = 30
+
+result = add_three_numbers(num1, num2, num3)
+print("The sum is:", result)
