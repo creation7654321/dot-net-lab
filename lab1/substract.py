1fcf2bcd8d6074093d7a06090e6212e96768b564
creation7654321 <creation.pradhan@gmail.com>
2025-03-22 21:19:14 +0545

    adding substact in develop branch

---
+++ lab1/substract.py
@@ -0,0 +1,11 @@
+# Function to subtract three numbers
+def subtract_three_numbers(a, b, c):
+    return a - b - c
+
+# Example usage
+num1 = 50
+num2 = 20
+num3 = 10
+
+result = subtract_three_numbers(num1, num2, num3)
+print("The result is:", result)
