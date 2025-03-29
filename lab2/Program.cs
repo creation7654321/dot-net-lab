2d0ea9985f5c1a8ce5864058aa0a9c1dd094cf5a
creation7654321 <creation.pradhan@gmail.com>
2025-03-29 18:49:02 +0545

    oddeven, swap , largest

---
+++ lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using lab2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        Largest compare = new Largest();
+        compare.largest(1, 10, 3);
+        Console.ReadKey();
+
+
+        OddEven oddEven = new OddEven();
+        oddEven.OddEvenfunc(2);
+        Console.ReadKey();
+
+        Swap SwapNumb = new Swap();
+        SwapNumb.Swapping();
+        Console.ReadKey();
+
+
+
+
+
+    }
+
+   
+}
